5dfed062b5fb6817f2c4fc70431180ef186beec7
Sergy2000 <sergen2000@gmail.com>
2025-05-13 16:09:18 +0200

    updated GUI

---
+++ GitDashDemo/GitUserModelcs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDashDemo
+{
+	internal class GitUserModelcs
+	{
+		public string login { get;set; }
+		public string avatar_url { get;set; }
+		public string repos_url { get; set; }
+		public string bio { get; set; }
+		public DateTime? created_at { get; set; }
+
+	}
+}
